c1d88847473e45ece49d216899d0161294b8e512
Hyunjoon Sung <sunghj1118@gmail.com>
2023-01-24 00:09:04 +0900

    Solved 2331 in python

--- brute force_sorting/2231.py
+++ brute force_sorting/2231.py
@@ -1,1 +1,11 @@
 N = int(input())
+M = 0
+
+for i in range(1, N+1):
+    current = list(map(int, str(i)))
+    sum_curr = i + sum(current)
+    if(sum_curr == N):
+        M = i
+        break
+
+print(M)
