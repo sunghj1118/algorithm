375fc08630efb05695bced2922d8ca347f9b60b7
Hyunjoon Sung <sunghj1118@gmail.com>
2023-01-31 09:25:50 +0900

    Solved 1003 using dynamic programming

--- binary_search_DP/1003.py
+++ binary_search_DP/1003.py
@@ -1,26 +1,13 @@
-count0 = 0
-count1 = 0
-
-
-def fibonacci(n):
-    global count0
-    global count1
-
-    if (n == 0):
-        count0 +=1
-        return 0
-    elif (n == 1):
-        count1 +=1
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
 T = int(input())
 values = []
 
 for i in range(T):
+    count0 = [1, 0]
+    count1 = [0, 1]
     n = int(input())
-    fibonacci(n)
-    print(count0, count1)
-    count0 = 0
-    count1 = 0
+    if n > 1:
+        for i in range(n-1):
+            count0.append(count1[-1])
+            count1.append(count0[-2]+count1[-1])
+
+    print(count0[n], count1[n])
