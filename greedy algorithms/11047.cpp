baa949e38a4a3fa36d8b410fd950e81417603095
Hyunjoon Sung <sunghj1118@gmail.com>
2023-03-04 23:21:04 +0900

    Found a better answer than mine for 11047

--- greedy algorithms/11047.cpp
+++ greedy algorithms/11047.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
+// fixed amount of coins
 int coins[11];
-int main() {
-    int n, k;
 
+int main() {
+    // taking input n amount of coins and k money to calculate
+    int n, k;
     cin >> n >> k;
+
+    // taking input of coins
     for (int i = 0; i < n; i++) {
         cin >> coins[i];
     }
 
-    int i = n - 1;
     int count=0;
-    while (i>=0) {
-        count += k / coins[i];
-        k = k % coins[i];
-        i--;
+    for(int i=n-1; i>=0; i--){
+        // adding how many coins fit into our money to the count
+        count += k/coins[i];
+    
+        // taking out the amount we used for our coins counted
+        k = k%coins[i];
     }
+    
+    // printing min amount of coins
     cout << count << endl;
 }
