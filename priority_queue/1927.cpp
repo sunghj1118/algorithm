1e0cd7c16a37f390111bd5817481bf2783ca0622
성현준 <sunghj1118@gmail.com>
2023-08-02 22:58:25 +0900

    add: 1927.cpp solved priority queue

---
+++ priority_queue/1927.cpp
@@ -0,0 +1,51 @@
+/*
+널리 잘 알려진 자료구조 중 최소 힙이 있다. 최소 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오.
+
+배열에 자연수 x를 넣는다.
+배열에서 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다.
+프로그램은 처음에 비어있는 배열에서 시작하게 된다.
+*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < N; i++)
+    {
+        int x;
+        cin >> x;
+
+        if (x == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << "\n";
+            }
+            else
+            {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
