f7f886de91b4a3b48c96d41eb2d714fe35474dd6
성현준 <60638615+sunghj1118@users.noreply.github.com>
2024-07-06 22:46:09 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 104 ms, Memory: 110780 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.py
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.py
@@ -1,14 +1,19 @@
+def smallbeat(k):
+	count = 0
+	while k != '0' * len(k):
+		k_int = int(k, 2)
+		k_int = k_int - (k_int & ((~k_int) + 1))
+		k = format(k_int, '0' + str(len(k)) + 'b')
+		count += 1
+	return count
+
+def largebeat(k):
+	return k.count('1')
+
 n = int(input())
 k = str(input())
 
-def dropthebeat(k):
-	k_int = int(k, 2)
-	result_int =  k_int-(k_int&((~k_int)+1))
-	return format(result_int, '0' + str(len(k)) + 'b')
-
-count = 0
-while k != '0' * len(k):
-	k = dropthebeat(k)
-	count += 1
-
-print(count)
+if n <= 30:
+	print(smallbeat(k))
+else:
+	print(largebeat(k))
