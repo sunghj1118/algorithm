d10e7820b96ff8be0af5c7b301a5d03039cb6722
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-06-26 07:32:27 +0900

    Time: 5 ms (52.49%), Space: 44.9 MB (78.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[] {i,j};
-                }
+        // insert into hash table
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length;i++){
+            map.put(nums[i], i);
+        }
+
+        // check if complement exists in table
+        for (int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
             }
         }
+
+
         return new int[]{};
     }
 }
