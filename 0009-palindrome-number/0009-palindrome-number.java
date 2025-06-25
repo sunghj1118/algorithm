101f35d188fe96e26c8fe975add7befdd6f161ba
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-06-26 08:37:07 +0900

    Time: 4 ms (100%), Space: 44.5 MB (18.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        // negative values are false
+        if (x<0) return false;
+
         int reverse = 0;
         int original = x;
-
-        // negative values are false
-        if (x<0){
-            return false;
-        }
 
         while(x!=0){
             reverse = reverse*10 + x%10;
             x /= 10;
         }
 
-        System.out.println(reverse + " " + original);
-        if(reverse == original){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return reverse == original;
     }
 }
