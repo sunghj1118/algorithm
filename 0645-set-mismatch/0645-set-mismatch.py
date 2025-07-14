f2978a97df894dc9b0fc0b8ca1b5fb96186a4089
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-07-14 21:03:17 +0900

    Time: 15 ms (54.28%), Space: 19 MB (67.49%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,11 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         counts = Counter(nums)
-        duplicate = counts.most_common(1)[0][0]
+        duplicate = None
+        loss = None
         
-        unique = set(nums)
         for i in range(1, len(nums)+1):
-            if i not in unique:
+            if counts[i] == 2:
+                duplicate = i
+            elif counts[i] == 0:
                 loss = i
         
         return [duplicate, loss]
