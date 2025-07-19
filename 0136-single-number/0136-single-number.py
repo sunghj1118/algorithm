9b6808d3c8a66dc224e5d0dcfe93cd66c45d2669
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-07-19 11:56:55 +0900

    Time: 7 ms (33.6%), Space: 20.1 MB (6.82%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,3 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return Counter(nums).most_common()[-1][0]
+        counts = set()
+        for i in range(len(nums)):
+            if nums[i] in counts:
+                counts.remove(nums[i])
+            else:
+                counts.add(nums[i])
+        
+        return counts.pop()
+        
+
