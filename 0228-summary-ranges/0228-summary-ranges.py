7d211632352858363fcb52a1e70bf3dc4e02f832
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-07-22 16:45:19 +0900

    Time: 0 ms (100%), Space: 17.8 MB (33.52%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        
+        ranges = []
+        start, end = nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            # diff larger than 1
+            if nums[i] > end+1:
+                if start == end:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(str(start) + "->" + str(end))
+                start,end = nums[i], nums[i]
+            else: # diff is 1
+                end = nums[i]
+        
+        # last index
+        if start==end:
+            ranges.append(str(start))
+        else:
+            ranges.append(str(start) + "->" + str(end))
+        
+        return ranges
