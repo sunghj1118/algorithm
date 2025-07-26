49224fcfa91233ed8b97ee06728b894cff3a9f22
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-07-26 11:59:10 +0900

    Time: 0 ms (100%), Space: 18.6 MB (14.5%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.py
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -1,15 +1,13 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        arr2 = [0 for _ in range(len(nums))]
-        even_pointer= 0
-        odd_pointer = len(nums)-1
+        left, right = 0, len(nums)-1
+
+        while left<right:
+            if nums[left]%2==1 and nums[right]%2==0:
+                nums[left], nums[right] = nums[right], nums[left]
+            if nums[left]%2==0:
+                left += 1
+            if nums[right]%2==1:
+                right -= 1
         
-        for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                arr2[even_pointer] = nums[i]
-                even_pointer += 1
-            else:
-                arr2[odd_pointer] = nums[i]
-                odd_pointer -= 1
-        
-        return arr2
+        return nums
