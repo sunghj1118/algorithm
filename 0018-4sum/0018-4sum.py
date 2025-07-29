f551735fb52527811981d6fbf234768487f84d19
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-07-29 15:57:45 +0900

    Time: 16 ms (97.23%), Space: 17.7 MB (82.84%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -10,16 +10,43 @@ class Solution:
             return [nums]
         
         for i in range(n-3):
+            # skip duplicates
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            # pruning: break early
+            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3] > target:
+                break
+            if nums[i]+nums[n-1]+nums[n-2]+nums[n-3] < target:
+                continue
+            
+
             for j in range(i+1,n-2):
+                # skip duplicates
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                if nums[i]+nums[j]+nums[j+1]+nums[j+2]>target:
+                    break
+                if nums[i]+nums[j]+nums[n-1]+nums[n-2]<target:
+                    continue
+
+
+
                 left, right = j+1, n-1
 
                 while left<right:
                     total = nums[i] + nums[j] + nums[left] + nums[right]
-                    comb = [nums[i], nums[j], nums[left], nums[right]]
 
-                    if total == target and comb not in result:
+                    if total == target:
                         result.append([nums[i], nums[j], nums[left], nums[right]])
                         left += 1
+                        right -= 1
+
+                        # skip duplicates
+                        while left<right and nums[left]==nums[left-1]:
+                            left += 1
+                        while left<right and nums[right]==nums[right+1]:
+                            right -= 1
+
                     elif total < target:
                         left += 1
                     else:
