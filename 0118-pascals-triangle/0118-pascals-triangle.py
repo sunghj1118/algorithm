81b2c4b01d3f8c4152536bad026bfc1469eadc23
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-08-01 13:39:31 +0900

    Time: 0 ms (100%), Space: 17.9 MB (36.9%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,14 +1,15 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        pyramid = []
-        for i in range(numRows):
-            row = [1]
+        if numRows == 0:
+            return [[]]
+        if numRows == 1:
+            return [[1]]
 
-            if i > 0:
-                for j in range(1, i):
-                    row.append(pyramid[i-1][j] + pyramid[i-1][j-1])
-                row.append(1)
-            
+        pyramid = [[1]]
+        for i in range(1,numRows):
+            row = [1] * (i+1)
+            for j in range(1,i):
+                row[j] = pyramid[i-1][j-1] + pyramid[i-1][j]
             pyramid.append(row)
         
         return pyramid
