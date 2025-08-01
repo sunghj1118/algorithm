cb780791744fa99fef3118615e70e411496fc2e6
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-08-01 13:07:36 +0900

    Time: 3 ms (47.75%), Space: 18 MB (10.03%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.py
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.py
@@ -1,6 +1,3 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        s_index = {ch:i for i,ch in enumerate(s)}
-        t_index = {ch:i for i,ch in enumerate(t)}
-        
-        return sum(abs(t_index[ch] - s_index[ch]) for ch in s)
+        return sum(abs(t.index(ch) - s.index(ch)) for ch in s)
