765d6844cb35bedf3b81feb1b72023bad662d17c
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-08-08 10:17:48 +0900

    Time: 1 ms (24.97%), Space: 17.9 MB (69.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -4,18 +4,18 @@ class Solution:
         original_color = image[sr][sc]
         if original_color == color:
             return image
-        
-        def dfs(r, c):
-            if (r < 0 or r >= rows or
-                c < 0 or c >= cols or
-                image[r][c] != original_color):
-                return
-            image[r][c] = color
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
 
+        stack = [(sr,sc)]
+
+        while stack:
+            r,c = stack.pop()
+
+            if (r<0 or r>= rows or c < 0 or c >= cols or image[r][c] != original_color):
+                continue
+            image[r][c] = color
+            stack.append((r+1, c))
+            stack.append((r-1, c))
+            stack.append((r, c+1))
+            stack.append((r, c-1))
         
-        dfs(sr, sc)
         return image
