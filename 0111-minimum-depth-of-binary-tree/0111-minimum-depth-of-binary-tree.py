235f7591993674a8ddc6e9be4df03df892d4d009
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-08-12 19:03:43 +0900

    Time: 0 ms (100%), Space: 49.9 MB (69.95%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -9,15 +9,15 @@ class Solution:
         if not root:
             return 0
         
-        min_depth = float('inf')
-        stack = [(root, 1)]
+        queue = deque([(root, 1)])
 
-        while stack:
-            node, depth = stack.pop()
-            if node:
-                if not node.left and not node.right:
-                    min_depth = min(min_depth, depth)
-                stack.append((node.left, depth+1))
-                stack.append((node.right, depth+1))
-        
-        return min_depth
+        while queue:
+            node, depth = queue.popleft()
+
+            if not node.left and not node.right:
+                return depth
+            
+            if node.left:
+                queue.append((node.left, depth+1))
+            if node.right:
+                queue.append((node.right, depth+1))
