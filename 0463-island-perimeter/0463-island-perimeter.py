48a6111d600fc8e3cf2c2d56b0eae980ec538e00
Hyunjoon Sung <60638615+sunghj1118@users.noreply.github.com>
2025-08-18 22:30:10 +0900

    Time: 30 ms (79.11%), Space: 18.1 MB (82.98%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,35 +2,18 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        visited = set()
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
-
-        def bfs(r,c):
-            queue = deque([(r,c)])
-            visited.add((r,c))
-            local_perimiter = 0
-
-            while queue:
-                r,c = queue.popleft()
-
-                for dr,dc in directions:
-                    nr,nc = r+dr, c+dc
-
-                    if 0<=nr<rows and 0<=nc<cols:
-                        if grid[nr][nc]==0:
-                            local_perimiter+=1
-                        elif grid[nr][nc]==1 and (nr,nc) not in visited:
-                            visited.add((nr,nc))
-                            queue.append((nr,nc))
-                    else:
-                        local_perimiter+=1
-            
-            return local_perimiter
-
+        perimeter = 0
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    return bfs(i,j)
+                if grid[i][j] == 1:
+                    if i == 0 or grid[i-1][j]==0:
+                        perimeter += 1
+                    if i == rows-1 or grid[i+1][j]==0:
+                        perimeter += 1
+                    if j == 0 or grid[i][j-1]==0:
+                        perimeter += 1
+                    if j == cols-1 or grid[i][j+1]==0:
+                        perimeter += 1
 
-        return 0
+        return perimeter
